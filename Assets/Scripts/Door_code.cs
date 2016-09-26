cb740df38d91896df2e1e2665345849e2eb8147e
jeremyBor <jere.bor@free.fr>
2016-09-26 15:35:12 +0200

    avec les ressourses c'est mieux

---
+++ Assets/Scripts/Door_code.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door_code : MonoBehaviour
+{
+    public GameObject[] m_cilindres;
+    private int[] m_code;
+
+	void Start ()
+    {
+        m_code =new int [4] { 7,3,4,8};
+	}
+
+    public void valueChange()
+    {
+        if (m_cilindres[0].GetComponent<Cylinder_code>().getValue() == m_code[0]
+            && m_cilindres[1].GetComponent<Cylinder_code>().getValue() == m_code[1]
+            && m_cilindres[2].GetComponent<Cylinder_code>().getValue() == m_code[2]
+            && m_cilindres[3].GetComponent<Cylinder_code>().getValue() == m_code[3])
+        {
+            Debug.Log("good");
+        }
+    }
+}
