72bf83243ad0ff371302d5cd8674549e96ba5ea7
YoueIUT <YoueIUT>
2016-12-16 11:23:49 +0100

    + Door animation * Digit Animation instead of Animator * Arrangement * Fix Door model pivot & other

--- Assets/Scripts/_InteractiveObject/Specific/Digit.cs
+++ Assets/Scripts/_InteractiveObject/Specific/Digit.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Digit : ActivableObject
 {
-    Animator m_animator;
+    Animation m_animation;
     int m_value = 0;
 
     Vector3 m_rotationAxis;
@@ -16,17 +16,17 @@ public class Digit : ActivableObject
     void Start()
     {
         m_rotationAxis = transform.right;
-        m_animator = GetComponent<Animator>();
+        m_animation = GetComponent<Animation>();
     }
 
     protected override void specificActivation()
     {
-        m_animator.Play("Rotate 36");
+        transform.Rotate(m_rotationAxis, -36);
+        m_animation.Play();
     }
 
     protected override void specificDeactivation()
     {
-		transform.Rotate(m_rotationAxis, -36);
         m_value = (m_value+1) % 10;
     }
 }
--- Assets/Scripts/_InteractiveObject/Specific/Lock.cs
+++ Assets/Scripts/_InteractiveObject/Specific/Lock.cs
@@ -8,16 +8,23 @@ public class Lock : ActivableObject
     [SerializeField]
     Digit[] m_digits = new Digit[4];
 
+    Animation m_animation;
+
 	void Start()
 	{
 		updateState = false;
-	}
+
+        m_animation = GetComponent<Animation>();
+
+    }
 
     /// <summary>
     /// Return true if correct.
     /// </summary>
     bool checkCode()
     {
+        m_animation.Play("Door_Pivot");
+
         for (int digitID = 0; digitID < 4; digitID++)
 		{
 			if (m_digits[digitID].Value != m_code[digitID])
@@ -32,12 +39,13 @@ public class Lock : ActivableObject
     /// </summary>
     void unlock()
     {
-        Destroy(gameObject);
+        m_animation.Play("Door_Open");
+        canBeActivated = false;
     }
 
     protected override void specificActivation()
     {
-		if (checkCode())
+        if (checkCode())
 		{
 			foreach (Digit digit in m_digits)
 				digit.canBeActivated = false;
