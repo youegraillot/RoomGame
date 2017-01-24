84e296e7886d7d8e2adbc37ef406c16af5857544
Youe Graillot <Youe.Graillot@etu.udamail.fr>
2017-01-24 11:34:27 +0100

    Animation feu
    + LightFlicker
    - Clean

---
+++ Assets/Scripts/Utilities/LightFlicker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class LightFlicker : MonoBehaviour
+{
+    [SerializeField, Range(0,1)]
+    float m_minIntensity = 0.25f;
+    [SerializeField, Range(0, 1)]
+    float m_maxIntensity = 0.5f;
+    [SerializeField, Range(0, 10)]
+    float m_speed = 2f;
+
+    Light m_light;
+    float m_randomSeed;
+
+    void Start()
+    {
+        m_light = GetComponent<Light>();
+        m_randomSeed = Random.Range(0.0f, 65535.0f);
+    }
+
+    void Update()
+    {
+        float noise = Mathf.PerlinNoise(m_randomSeed, Time.time* m_speed);
+        m_light.intensity = Mathf.Lerp(m_minIntensity, m_maxIntensity, noise);
+    }
+}
