2eb39dd96fcec8e2db1ad72f460c2b425bffe7d0
allastal <ryujiixx@gmail.com>
2017-01-25 09:18:12 +0100

    ajout main menu

---
+++ Assets/MAINMENU_TEMP/ButtonComponent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+public class ButtonComponent : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public float alphaVal = 90f;
+    GameObject m_children;
+    void Start()
+    {
+        m_children = transform.GetChild(0).gameObject;
+        m_children.SetActive(false);
+
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        m_children.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        m_children.SetActive(false);
+    }
+}
